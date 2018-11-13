ee801185bb4c20bfeab8cbcb2e7775f37c12f0fc
Steve Warren <steve@interactivenyc.com>
2018-11-13 12:44:30 -0500

    removing cruft

--- app/socket.js
+++ app/socket.js
@@ -4,15 +4,6 @@ const socket = io(window.location.origin);
 
 socket.on('connect', () => {
     console.log('[ socket client ] Connected!', socket.id);
-    socket.emit('register-ship');
-});
-
-socket.on('new-game-id', (gameId, enemies) => {
-    console.log('[ socket client ] new-game-id!', gameId, enemies);
-});
-
-socket.on('new-enemy', (enemy) => {
-    console.log('[ socket client ] new-enemy!', enemy);
 });
 
 export default socket;
--- server/socket/index.js
+++ server/socket/index.js
@@ -1,5 +1,4 @@
 module.exports = (io) => {
-    let gameId = 0;
     let ships = {};
 
     io.on('connection', (socket) => {
@@ -9,22 +8,9 @@ module.exports = (io) => {
             console.log(`Connection ${socket.id} has left the building`);
             delete ships[socket.id];
             socket.broadcast.emit('enemy-left', socket.id);
-            console.log('[ socket server ] ships left\n', ships);
-        });
-
-        socket.on('register-ship', () => {
-            console.log('[ socket server ] register ship', socket.id);
-            gameId++;
-
-            socket.emit('new-game-id', gameId, ships);
-
-            ships[socket.id] = { gameId };
-            socket.broadcast.emit('new-enemy', ships[socket.id]);
         });
 
         socket.on('ship-update', (shipInfo) => {
-            // console.log('[ socket server ] ship-update:', ships[socket.id].gameId);
-
             ships[socket.id] = { ...ships[socket.id], shipInfo };
             socket.broadcast.emit('update-ships', ships);
         });
